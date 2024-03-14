03f6c33f93ebc71fd711621802c24dcc4f11ac0e
PMartinezVega <pedromv1991@gmail.com>
2024-03-13 20:53:31 -0500

    actualizacion

--- Pre_entrega_martinez/src/App.jsx
+++ Pre_entrega_martinez/src/App.jsx
@@ -3,16 +3,21 @@ import ItemCount from './components/ItemCount/ItemCount'
 import Navbar from './components/Navbar/Navbar'
 import ItemListContainer from './components/itemListContainer/itemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
+import{BrowserRouter,Routes,Route} from "react-router-dom"
 
 function App() {
   
 
   return (
     <>
+     <BrowserRouter>
       <Navbar/>
-      <ItemListContainer greeting={"Bienvenidos"}/>
-      <ItemDetailContainer/>
-
+      <Routes>
+        <Route path="/" element={<ItemListContainer greeting={"Bienvenidos"}/>}/>
+        <Route path="/category/:categoryId" element={<ItemListContainer greeting={"Listado de productos filtrados"}/>}/>
+        <Route path="/item/:itemId" element={<ItemDetailContainer/>}/>
+      </Routes>
+      </BrowserRouter> 
     </>
   )
 }
--- Pre_entrega_martinez/src/components/Navbar/Navbar.jsx
+++ Pre_entrega_martinez/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
+import CartWidget from '../CartWidget/CartWidget'
+import { Link, useNavigate } from 'react-router-dom'
+
+
 const Navbar=()=>{
     return(
         <Header style= {{display:"flex", justifiContent:"space-between",alignItems:"center"}}>
             <h1>Ecommerce</h1>
             <nav>
-                <a >Celulares</a>
-                <a >Tablets</a>
-                <a >Notebooks</a>
+            <Link to="/category/celular">Celulares</Link>
+            <Link to="/category/tablet">Tablets</Link>
+            <Link to="/category/notebook">Notebooks</Link>
 
             </nav>
             <CartWidget/>
